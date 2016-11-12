607e42b9254a7d7d342af679239c842f1cf4a70e
trickbooter <paul@trickbooter.com>
2016-11-12 22:01:27 +1100

    Add publish scripts

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,20 @@
-# Create a new orphand branch (no commit history) named gh-pages
-git checkout --orphan gh-pages
+#!/bin/bash
 
-# Unstage all files
-git rm --cached $(git ls-files)
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Grab one file from the master branch so we can make a commit
-git checkout master README.md
-
-# Add and commit that file
-git add .
-git commit -m "INIT: initial commit on gh-pages branch"
-
-# Push to remote gh-pages branch
-git push origin gh-pages
-
-# Return to master branch
-git checkout master
-
-# Remove the public folder to make room for the gh-pages subtree
-rm -rf public
-
-# Add the gh-pages branch of the repository. It will look like a folder named public
-git subtree add --prefix=public git@github.com:trickbooter/trickbooter.github.io.git gh-pages --squash
-
-# Pull down the file we just committed. This helps avoid merge conflicts
-git subtree pull --prefix=public git@github.com:trickbooter/trickbooter.github.io.git gh-pages
-
-# Run hugo. Generated site will be placed in public directory (or omit -t ThemeName if you're not using a theme)
+# Build the project.
 hugo
 
-
-# Add everything
+# Add changes to git.
 git add -A
 
-# Commit and push to master
-git commit -m "Updating site" && git push origin master
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 
-# Push the public subtree to the gh-pages branch
+# Push source and build repos.
+git push origin master
 git subtree push --prefix=public git@github.com:trickbooter/trickbooter.github.io.git gh-pages
