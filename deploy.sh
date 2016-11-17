334407200b95ba73c7a3eb88bf027804e7f18eba
trickbooter <paul@trickbooter.com>
2016-11-17 15:06:47 +1100

    rebuilding site Thu 17 Nov 2016 15:06:47 AEDT

--- deploy.sh
+++ deploy.sh
@@ -17,5 +17,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin develop
-git subtree pull --prefix=public origin master
+git -C public pull origin master --allow-unrelated-histories
 git subtree push --prefix=public origin master
